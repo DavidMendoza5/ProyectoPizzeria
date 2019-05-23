0ea3596ef80f8ac4eadf574dff0b7be0e1b16b4b
DavidMendoza <cruz-azul99@hotmail.com>
2019-05-22 20:25:42 -0500

    Main file was created to package controller

---
+++ src/controlador/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+import vista.*;
+
+/**
+ *
+ * @author david
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LogIn log = new LogIn();
+        log.setVisible(true);
+        log.setLocationRelativeTo(null);
+        
+    }
+    
+}
